7af8ab902a68fc04ad81c99eacdaad66ac5aaebb
Farhan Hafeez <farhanhafeez985@gmail.com>
2021-07-08 04:49:09 +0500

    third

--- FYP_Ostore/settings.py
+++ FYP_Ostore/settings.py
@@ -152,5 +152,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
